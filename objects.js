874dbf9e2248570a4e0898ec1e145d73fc6a2641
Tom De Vries <tdevries60@gmail.com>
2018-06-16 22:40:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ var playlist = {
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({playlist}, artistName, songTitle);
+  return Object.assign(playlist, artistName, songTitle);
 }
